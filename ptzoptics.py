74c43353edde8bfed594b1fc18df0b4ccc10ca89
Cameron Bowen <39662961+wcameronbowen@users.noreply.github.com>
2021-08-01 22:30:24 -0500

    adding some more functions and testing putting it all together as well as some error handling and setting a timeout on connections that don't receive a response

--- ptzoptics.py
+++ ptzoptics.py
@@ -1,24 +1,52 @@
 import socket
+socket.setdefaulttimeout(5)
 
-def recallPreset(host, port, command):
-    try: 
-        #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-        #s.connect((host, port))
-        #data = bytes.fromhex(command)
-        #r = s.send(data)
-        #s.close()
-        #print(r)
-        #return(r)
+def openConnection(host, port, command):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.settimeout(5)
+        s.connect((host, port))
+        s.send(bytes.fromhex(command))
+        r = s.recv(1024)
+    return(r)
 
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.connect((host, port))
-            data = bytes.fromhex(command)
-            s.send(data)
-            r = s.recv(1024)
+def recallPreset(host, port, presetNumber):
+    if len(presetNumber) == 2:
+        command = f"8101043F02{presetNumber}FF"
+    else:
+        command = f"8101043F020{presetNumber}FF"
+    return(openConnection(host, port, command))
 
-        print('Received', repr(r))
-        return(r)
+def setPreset(host, port, presetNumber):
+    if len(presetNumber) == 2:
+        command = "8101043F01{presetNumber}FF"
+    else:
+        command = "8101043F010{presetNumber}FF"
+    return(openConnection(host, port, command))
 
-    except:
-        print("there was an error")
+def getPanTiltPosition(host, port):
+    command = "81090612FF"
+    return(openConnection(host, port, command))
 
+def getZoomPosition(host, port):
+    command = "81090447FF"
+    return(openConnection(host, port, command))
+
+def setPanTiltPosition(host, port, pan, tilt):
+    command = f"810106021410{pan}{tilt}FF"
+    return(openConnection(host, port, command))
+
+def setZoomPosition(host, port, zoom):
+    command = f"81010447{zoom}FF"
+    return(openConnection(host, port, command))
+
+def convertPan(r):
+    pan = r.hex()[4:12]
+    return(pan)
+
+def convertTilt(r):
+    tilt = r.hex()[12:20]
+    return(tilt)
+
+def convertZoom(r):
+    zoom = r.hex()[4:12]
+    return(zoom)
--- runme.py
+++ runme.py
@@ -1,14 +1,52 @@
 from ptzoptics import *
 import socket
+import json
 
-#variables
-hosts = ['172.20.1.50']
+#module settings
+socket.setdefaulttimeout(5)
+
+#static variables
 port = 1259
-presetNumber = input("what preset do you want to recall (e.g. 01): ")
-command = f'8101043F02{presetNumber}FF'
+
+#parse json
+print("parsing json input")
+f = open("camera-config.json",)
+data = json.load(f)
+
+#intialize arrays
+hosts = []
+for object, details in data.items():
+    print(f"loading {object}")
+    hosts.append(details["ip"])
+
 
 
 #functions
 
+#for host in hosts:
+#
+#    r = setPosition(host, port, pan, tilt)
+#    if r.hex() == "904yFF" :
+#        print("command received")
+#    print(r.hex())
+
+
 for host in hosts:
-    recallPreset(host, port, command)
+    for preset in data["camera-1"]["presets"]:
+        try:
+            print(preset)
+            recallPreset(host, port, preset)
+            print(getPanTiltPosition(host, port).hex())
+            print(getZoomPosition(host, port).hex())
+        except socket.timeout:
+            print("doesn't seem to be anything listening")
+
+
+#for host in hosts:
+#    for preset in data["camera-1"]["presets"]:
+#        pan = preset["pan"]
+#        tilt = preset["tilt"]
+#        zoom = preset["zoom"]
+#        setPanTiltPosition(host, port, pan, tilt)
+#        setZoomPosition(host, port, zoom)
+#        setPreset(host, port, preset)
